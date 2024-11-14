c6ee5c22ec73cf28d6f26ce4d4a3dbc03ee3bd6a
cyt on macbook air <yitingchen1228@gmail.com>
2024-11-14 03:55:29 -0500

    Feature Enginner和train test split完成

--- DataSource.py
+++ DataSource.py
@@ -8,92 +8,115 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
-def main():
-    # Load the dataset
-    data_path = './data/nasdq.csv'
-    data = pd.read_csv(data_path)
+class DataSet(object):
+    def __init__(self):
+        path = "./data/nasdq.csv"
+        self.inited = False
+        #init the data
+        data = pd.read_csv(path)
 
-    print(data.head())
+        # Date column: Convert to datetime format
+        data['Date'] = pd.to_datetime(data['Date'])
+        data.set_index('Date', inplace=True)
 
-    # Check for missing values and data types
-    data.info()
+        # Rename columns to remove spaces
+        data.columns = data.columns.str.replace(' ', '')
 
-    # Check for null values and basic statistics
-    data.describe()
-    data.isnull().sum()
+        # Feature Engineer
+        # Add features such as daily returns and rolling averages
+        data['Daily_Return'] = data['Close'].pct_change()  # pct_change() 计算 Close 列中每天价格的百分比变化，即每日收益率。
+        # rolling(window=30) 对数据进行滚动窗口操作，这里窗口大小为30，表示过去30天的数据。 std() 计算每个30天窗口内 Close 价格的标准差，即波动率。
+        data['Volatility'] = data['Close'].rolling(window=30).std()
+        # rolling(window=30) 进行滚动窗口操作。 mean() 计算每个30天窗口内 Close 价格的均值。
+        data['Rolling_Mean_Close'] = data['Close'].rolling(window=30).mean()
+        data.dropna(inplace=True)
 
-    # Date column: Convert to datetime format
-    data['Date'] = pd.to_datetime(data['Date'])
-    data.set_index('Date', inplace=True)
+        self.data = data
 
-    print(data.head())
+        # Train-Test Split and Scaling
+        # Define target variable and features
+        X = data.drop(['Close'], axis=1)  # Ensure 'Close' is dropped to create the feature set
+        y = data['Close']  # Target variable is 'Close' price
 
-    # Rename columns to remove spaces
-    data.columns = data.columns.str.replace(' ', '')
+        # Step 1: Replace infinite values with NaN
+        X.replace([np.inf, -np.inf], np.nan, inplace=True)
 
-    # Plot Open, High, Low, Close over time
-    fig, ax = plt.subplots(2, 2, figsize=(16, 12))
-    data['Open'].plot(ax=ax[0, 0], title="Opening Price Over Time")
-    data['High'].plot(ax=ax[0, 1], title="Highest Price Over Time")
-    data['Low'].plot(ax=ax[1, 0], title="Lowest Price Over Time")
-    data['Close'].plot(ax=ax[1, 1], title="Closing Price Over Time")
-    plt.tight_layout()
-    plt.show()
+        # Step 2: Check for NaN values and handle them
+        # Using forward fill to handle NaN values (you can adjust this as needed)
+        X.fillna(method='ffill', inplace=True)
 
-    # Correlation Matrix
-    correlation_matrix = data.corr()
-    plt.figure(figsize=(12, 8))
-    sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", fmt=".2f")
-    plt.title("Correlation Matrix of NASDAQ Dataset")
-    plt.show()
+        self.X = X
+        self.y = y
 
-    #Feature Engineer
-    # Add features such as daily returns and rolling averages
-    data['Daily_Return'] = data['Close'].pct_change() #	pct_change() 计算 Close 列中每天价格的百分比变化，即每日收益率。
-    #rolling(window=30) 对数据进行滚动窗口操作，这里窗口大小为30，表示过去30天的数据。 std() 计算每个30天窗口内 Close 价格的标准差，即波动率。
-    data['Volatility'] = data['Close'].rolling(window=30).std()
-    #rolling(window=30) 进行滚动窗口操作。 mean() 计算每个30天窗口内 Close 价格的均值。
-    data['Rolling_Mean_Close'] = data['Close'].rolling(window=30).mean()
-    data.dropna(inplace=True)
+        # Step 3: Split the data into training and testing sets
+        train_size = int(len(data) * 0.8)  # 80% 用于训练
+        X_train, y_train = X[:train_size], y[:train_size]
+        X_test, y_test = X[train_size:], y[train_size:]
+        # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)  #random split the data, not appropriate for time series data
 
-    # Display the new features
-    print(data[['Close', 'Daily_Return', 'Volatility', 'Rolling_Mean_Close']].head())
+        # Step 4: Standardize the data
+        # 标准化数据：x'=(x-u)/sigma, u is the mean, sigma is the standard deviation
+        scaler = StandardScaler()
+        # fit_transform() 方法用于在训练集 X_train 上计算标准化所需的均值和标准差，并使用这些统计量对 X_train 进行标准化。
+        # fit() 计算训练集的均值和标准差。transform() 根据计算的均值和标准差将训练数据转换为标准化后的数据。
+        X_train_scaled = scaler.fit_transform(X_train)
+        # transform() 方法用于使用在 X_train 上计算的均值和标准差来标准化测试集 X_test。注意，这里没有调用 fit()，因为测试集的标准化应该基于训练集的统计信息，而不是重新计算。
+        X_test_scaled = scaler.transform(X_test)
+        print("Data scaling successful!")
 
-    #Train-Test Split and Scaling
-    # Define target variable and features
-    X = data.drop(['Close'], axis=1)  # Ensure 'Close' is dropped to create the feature set
-    y = data['Close']  # Target variable is 'Close' price
+        self.X_train = X_train_scaled
+        self.X_test = X_test_scaled
+        self.y_train = y_train
+        self.y_test = y_test
 
-    # Step 1: Replace infinite values with NaN
-    X.replace([np.inf, -np.inf], np.nan, inplace=True)
+        self.inited = True
+        print("initializing successfully!")
 
-    # Step 2: Check for NaN values and handle them
-    # Using forward fill to handle NaN values (you can adjust this as needed)
-    X.fillna(method='ffill', inplace=True)
 
-    # Step 3: Split the data into training and testing sets
-    train_size = int(len(data) * 0.8)  # 80% 用于训练
-    X_train, y_train = X[:train_size], y[:train_size]
-    X_test, y_test = X[train_size:], y[train_size:]
-    #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)  #random split the data, not appropriate for time series data
+    def EDA(self): #展示数据的特征
+        data = self.data
+        print(data.head())
 
-    # Step 4: Standardize the data
-    #标准化数据：x'=(x-u)/sigma, u is the mean, sigma is the standard deviation
-    scaler = StandardScaler()
-    #fit_transform() 方法用于在训练集 X_train 上计算标准化所需的均值和标准差，并使用这些统计量对 X_train 进行标准化。
-    #fit() 计算训练集的均值和标准差。transform() 根据计算的均值和标准差将训练数据转换为标准化后的数据。
-    X_train_scaled = scaler.fit_transform(X_train)
-    #transform() 方法用于使用在 X_train 上计算的均值和标准差来标准化测试集 X_test。注意，这里没有调用 fit()，因为测试集的标准化应该基于训练集的统计信息，而不是重新计算。
-    X_test_scaled = scaler.transform(X_test)
+        # Check for missing values and data types
+        data.info()
 
-    print("Data scaling successful!")
+        # Check for null values and basic statistics
+        data.describe()
+        data.isnull().sum()
+
+        # Plot Open, High, Low, Close over time
+        fig, ax = plt.subplots(2, 2, figsize=(16, 12))
+        data['Open'].plot(ax=ax[0, 0], title="Opening Price Over Time")
+        data['High'].plot(ax=ax[0, 1], title="Highest Price Over Time")
+        data['Low'].plot(ax=ax[1, 0], title="Lowest Price Over Time")
+        data['Close'].plot(ax=ax[1, 1], title="Closing Price Over Time")
+        plt.tight_layout()
+        plt.show()
+
+        # Correlation Matrix
+        correlation_matrix = data.corr()
+        plt.figure(figsize=(12, 8))
+        sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", fmt=".2f")
+        plt.title("Correlation Matrix of NASDAQ Dataset")
+        plt.show()
+
+    def train_and_test(self):
+        if self.inited:
+            return self.X_train, self.X_test, self.y_train, self.y_test
+        else:
+            raise RuntimeError('Dataset not yet initialized!')
+
+
+def test():
+    dataset = DataSet()
+    X_train, X_test, y_train, y_test = dataset.train_and_test()
 
     # Train a baseline model
     lr = LinearRegression()
-    lr.fit(X_train_scaled, y_train)
+    lr.fit(X_train, y_train)
 
     # Make predictions
-    y_pred_lr = lr.predict(X_test_scaled)
+    y_pred_lr = lr.predict(X_test)
 
     # Evaluate the model
     print("Linear Regression R2 Score:", r2_score(y_test, y_pred_lr))
@@ -101,4 +124,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    test()
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
+1.
+我们调用数据集的时候统一用DataSource写的借口
+```python
+from DataSource import DataSet
 
+dataset = DataSet()
+X_train, X_test, y_train, y_test = dataset.train_and_test()
+```
+数据增加了"Daily_Return","'Volatility","Rolling_Mean_Close"三个feature  
+对每个feature进行了标准化处理  
+训练集和测试集划分是按照时间顺序8:2划分，比如10天就前8天为训练集，后2天为测试集
